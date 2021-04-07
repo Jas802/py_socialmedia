8da3044c91854dc6030311f05f8c711a7203eca6
Jas802 <jas.spencer414@gmail.com>
2021-04-07 12:45:53 -0400

    model for User Profile

--- pysocialmedia/settings.py
+++ pysocialmedia/settings.py
@@ -135,6 +135,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEIDA_URL = '/media/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
