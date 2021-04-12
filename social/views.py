f75e6ca5affc71661350a481b72afc916d69036e
Jas802 <jas.spencer414@gmail.com>
2021-04-12 13:39:13 -0400

    added follower logic to views

--- social/views.py
+++ social/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import UserPassesTestMixin, LoginRequiredMixin
 from django.views import View
@@ -110,14 +110,27 @@ class ProfileView(View):
     profile = UserProfile.objects.get(pk=pk)
     user = profile.user
     posts = Post.objects.filter(author=user).order_by('created_on')
+    
+    followers = profile.followers.all()
+
+    for follower in followers:
+      if follower == request.user:
+        is_following = True
+        break
+      else:
+        is_following = False
+
+    number_of_followers = len(followers)
 
     context = {
       'user': user,
       'profile': profile,
       'posts': posts,
+      'number_of_followers': number_of_followers,
+      'is_following': is_following,
     }
 
-    return render (request, 'social/profile.html', context)
+    return render(request, 'social/profile.html', context)
 
 class ProfileEditView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
   model = UserProfile
@@ -131,5 +144,21 @@ class ProfileEditView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
   def test_func(self):
     profile = self.get_object()
     return self.request.user == profile.user
+
+class AddFollower(LoginRequiredMixin, View):
+  def post(self, request, pk, *args, **kwargs):
+    profile = UserProfile.objects.get(pk=pk)
+    profile.followers.add(request.user)
+
+    return redirect('profile', pk=profile.pk)
+
+class RemoveFollower(LoginRequiredMixin, View):
+  def post(self, request, pl, *args, **kwargs):
+    profile = UserProfile.objects.get(pk=pk)
+    profile.followers.remove(request.user)
+
+    return redirect('profile', pk=profile.pk)
+
+
 
 
