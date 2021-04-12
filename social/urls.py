50720de49d38ce420f8596fcbb4183b36646263f
Jas802 <jas.spencer414@gmail.com>
2021-04-12 13:28:48 -0400

    added follower field to User model

--- social/urls.py
+++ social/urls.py
@@ -3,10 +3,10 @@ from .views import PostListView, PostDetailView, PostEditView, PostDeleteView, C
 
 urlpatterns = [
     path('', PostListView.as_view(), name='post-list'),
-    path('post/<int:pk>', PostDetailView.as_view(), name='post-detail'),
-    path('post/edit/<int:pk>', PostEditView.as_view(), name='post-edit'),
-    path('post/delete/<int:pk>', PostDeleteView.as_view(), name='post-delete'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/edit/<int:pk>/', PostEditView.as_view(), name='post-edit'),
+    path('post/delete/<int:pk>/', PostDeleteView.as_view(), name='post-delete'),
     path('post/<int:post_pk>/comment/delete/<int:pk>/', CommentDeleteView.as_view(), name='comment-delete'),
-    path('profile/<int:pk>', ProfileView.as_view(), name='profile'),
-    path('profile/edit/<int:pk>', ProfileEditView.as_view(), name='profile-edit'),
+    path('profile/<int:pk>/', ProfileView.as_view(), name='profile'),
+    path('profile/edit/<int:pk>/', ProfileEditView.as_view(), name='profile-edit'),
 ]
