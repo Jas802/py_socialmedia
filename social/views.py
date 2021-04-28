650bdeecca82b6655d25eeb206185c0d0a7cc720
Jas802 <jas.spencer414@gmail.com>
2021-04-28 15:00:09 -0400

    view and urls for clicking notifications

--- social/views.py
+++ social/views.py
@@ -3,7 +3,7 @@ from django.db.models import Q
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import UserPassesTestMixin, LoginRequiredMixin
 from django.views import View
-from .models import Post, Comment, UserProfile
+from .models import Post, Comment, UserProfile, Notification
 from .forms import PostForm, CommentForm
 from django.views.generic.edit import UpdateView, DeleteView
 from django.http import HttpResponseRedirect
@@ -328,3 +328,23 @@ class ListFollowers(View):
     }
 
     return render(request, 'social/followers_list.html', context)
+
+class PostNotification(View):
+  def get(self, request, notification_pk, post_pk, *args, **kwargs):
+    notification = Notification.objects.get(pk=notification_pk)
+    post = Post.objects.get(pk=post_pk)
+
+    notification.user_has_seen = True
+    notification.save()
+
+    return redirect('post-detail', pk=post_pk)
+
+class FollowNotification(View):
+  def get(self, request, notification_pk, profile_pk, *args, **kwargs):
+    notification = Notification.objects.get(pk=notification_pk)
+    profile = UserProfile.objects.get(pk=profile_pk)
+
+    notification.user_has_seen = True
+    notification.save()
+
+    return redirect('profile', pk=profile_pk)
